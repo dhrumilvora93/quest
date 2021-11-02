f1cd591db2840db6cb82f6b2a9bb619f04885d15
Dhrumil <dhrumilvora@Dhrumils-MBP-2.fios-router.home>
2021-11-02 10:04:36 -0400

    added secret word in env

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-yum install git -y && yum install docker -y
-service docker start
+sudo yum install git -y && sudo yum install docker -y
+sudo service docker start
 git clone https://github.com/dhrumilvora93/quest.git 
 cd quest/quest_code
+sudo usermod -a -G docker ec2-user
 docker build -t test .
 docker run -p 3000:3000 -t test 
